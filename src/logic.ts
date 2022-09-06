cdb88e18adb275a34abc8b89d9b021a5aa0ad8f3
Evgeny Arshinov <earshinov@gmail.com>
2022-09-06 12:38:36 +0300

    feat: Natural sort

--- src/logic.ts
+++ src/logic.ts
@@ -15,7 +15,7 @@ function compileMorph(this: void, morph: string): IMorph | null {
 
 export function run(this: void, editor: vscode.TextEditor, edit: vscode.TextEditorEdit, morph: IMorph, naturalSort: boolean) {
   const selections = unique(
-    sort(editor.selections.map((selection) => getSelectionData(editor.document, selection, morph))),
+    sort(editor.selections.map((selection) => getSelectionData(editor.document, selection, morph, naturalSort))),
   );
   const texts = sortTexts(selections);
   for (let i = selections.length - 1; i >= 0; --i) edit.replace(selections[i].line, texts[i]);
@@ -26,11 +26,14 @@ function getSelectionData(
   document: vscode.TextDocument,
   selection: vscode.Selection,
   morph: IMorph,
+  naturalSort: boolean,
 ): ISelectionData {
   const line = lineFromSelection(document, selection);
+  const comparisonText = morph(document.getText(selection));
+  const comparison = naturalSort ? prepareNaturalSort(comparisonText) : [comparisonText];
   return {
     selection: selection,
-    comparisonText: morph(document.getText(selection)),
+    comparison,
     line: line,
     lineText: document.getText(line),
   };
@@ -41,6 +44,26 @@ function lineFromSelection(this: void, document: vscode.TextDocument, selection:
   const lineStart = pos.with({ character: 0 });
   const lineEnd = document.lineAt(pos).range.end;
   return new vscode.Range(lineStart, lineEnd);
+}
+
+const RE_NUMBER = /-?\d+(\.\d+)?/g;
+
+type Chunk = string | number;
+
+function prepareNaturalSort(s: string): Chunk[] {
+  // Split the source string into character chunks and numeric chunks
+  let m: RegExpExecArray | null;
+  let i = 0;
+  const ret: Chunk[] = [];
+  while ((m = RE_NUMBER.exec(s))) {
+    if (i < m.index)
+      ret.push(s.substring(i, m.index));
+    ret.push(parseFloat(m[0]));
+    i = m.index + m[0].length;
+  }
+  if (i < s.length)
+    ret.push(s.substring(i));
+  return ret;
 }
 
 function sort(this: void, selections: ISelectionData[]) {
@@ -68,12 +91,25 @@ function unique(this: void, selections: ISelectionData[]) {
 function sortTexts(this: void, selections: ISelectionData[]) {
   return selections
     .slice() // clone
-    .sort((a, b) => compare(a.comparisonText, b.comparisonText))
+    .sort((a, b) => compare(a.comparison, b.comparison))
     .map((selection) => selection.lineText);
 }
 
-function compare(this: void, a: any, b: any) {
-  return a < b ? -1 : a > b ? 1 : 0;
+function compare(this: void, a: Chunk[], b: Chunk[]) {
+  for (let i = 0; i < a.length && i < b.length; ++i) {
+    // If different types, consider numbers less than character strings
+    if (typeof a[i] !== typeof b[i])
+      return typeof a[i] == "number" ? -1 : 1;
+
+    // Same-type comparison
+    if (a[i] < b[i])
+      return -1;
+    if (a[i] > b[i])
+      return 1;
+  }
+
+  // If one array is a prefix of another, the longer array is greater.
+  return a.length - b.length;
 }
 
 interface IMorph {
@@ -82,7 +118,7 @@ interface IMorph {
 
 interface ISelectionData {
   selection: vscode.Selection;
-  comparisonText: string;
+  comparison: Chunk[];
   line: vscode.Range;
   lineText: string;
 }
